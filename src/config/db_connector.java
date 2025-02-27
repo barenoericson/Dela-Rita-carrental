bb2bb3526fe50f5bf6e3566a3960113e58b85a9a
elgiejohn <ejdelarita@gmail.com>
2025-02-27 22:46:49 +0800

    task 1-4

---
+++ src/config/db_connector.java
@@ -0,0 +1,65 @@
+package config;
+
+import java.sql.*;
+
+public class db_connector {
+    private Connection conn;
+    
+    public db_connector(){
+        String url = "jdbc:mysql://localhost:3306/carrental";
+        String user = "root";
+        String password = "";
+        
+        try {
+            
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Can't Connect to Database: " + e.getMessage());
+        }
+        
+    }
+    
+    public ResultSet getData(String sql) throws SQLException{
+        Statement statement = conn.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        return result;
+    }
+    
+    public boolean fieldExists(String fieldName, String value) throws SQLException {
+        String sql = "SELECT * FROM user_table WHERE " + fieldName + " = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, value);
+        ResultSet result = pstmt.executeQuery();
+
+        return result.next(); 
+    }
+
+    
+    /*
+    private void display_data(){
+        try {
+            db_connector dbcon = new db_connector();
+            ResultSet result = dbcon.get_data("SELECT * FROM table_name");
+            table_name.setModel(DbUtils.resultSetToTableModel(result));
+        } catch (SQLException e) {
+            System.out.println("Can't Connect to Database: " + e.getMessage());
+        }
+        
+    }
+    */
+    public int insertData(String sql){
+        int result;
+        try{
+            try (PreparedStatement pst = conn.prepareStatement(sql)) {
+                pst.executeUpdate();
+                System.out.println("Inserted Successfully!");
+            }
+            result = 1;
+        }catch(SQLException e){
+            System.out.println("Connection Error: " + e.getMessage());
+            result = 0;
+        }
+        return result;
+    }
+    
+}
